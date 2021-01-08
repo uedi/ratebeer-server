738a4726885160f2b0e3c65843de29e487cb5dad
uedi <uedi@users.noreply.github.com>
2021-01-08 10:26:00 +0200

    configure, mongodb connection

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const app = express()
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('Could not connect to MongoDB:', error.message)
+    })
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello</h1>')
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require('./app')
 const http = require('http')
+const config = require('./utils/config')
 
 const server = http.createServer(app)
 
-const port = 3000
+console.log(`Env: ${process.env.NODE_ENV}`)
 
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
